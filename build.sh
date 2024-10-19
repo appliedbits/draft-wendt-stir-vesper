31a2017463745a69ae021098e109a3d6b597af76
Chris Wendt <chris@chriswendt.net>
2024-10-19 09:05:12 -0400

    .gitignore and build changes

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 TITLE="draft-wendt-stir-vesper"
-VERSION="-00"
+VERSION="-01"
 
-/opt/homebrew/Cellar/gem-kramdown-rfc/1.7.17/gems/kramdown-rfc2629-1.7.17/bin/kramdown-rfc $TITLE$VERSION.md > $TITLE$VERSION.xml
+/opt/homebrew/Cellar/gem-kramdown-rfc/1.7.19/gems/kramdown-rfc2629-1.7.19/bin/kramdown-rfc $TITLE$VERSION.md > $TITLE$VERSION.xml
 xml2rfc -q $TITLE$VERSION.xml -o $TITLE$VERSION.txt --text
 
 # /Users/cwendt/Sync/MyDocs/ietf
